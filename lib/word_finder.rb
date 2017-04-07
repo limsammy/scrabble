64bb7330ca4b78e9dc8cb076bcfa18837b7fb473
Sam Lim <limsammy1@gmail.com>
2017-04-07 10:46:39 -0600

    for_letters function of WordFinder works, passing tests

--- lib/word_finder.rb
+++ lib/word_finder.rb
@@ -2,5 +2,13 @@ class WordFinder
   def words
     File.read('/usr/share/dict/words').split("\n")
   end
-  
+
+  def for_letters(letters_collection)
+    possible_words = []
+    words.each { |line|
+      unless letters_collection.map { |c| line.include?(c) }.include? false
+        possible_words << line
+      end
+    }
+  end
 end
--- test/word_finder_test.rb
+++ test/word_finder_test.rb
@@ -5,5 +5,10 @@ require 'minitest/pride'
 require 'pry'
 
 class WordFinderTest < Minitest::Test
+  def test_for_letters_method_works
+    finder = WordFinder.new
+    possible_words = finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o'])
 
+    assert possible_words.include?("ape")
+  end
 end
